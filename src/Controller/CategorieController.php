b8b8ec9c8ee359cb99ff5e0ae243087a9e78b0cb
Raphaël <coursierap@icloud.com>
2026-03-04 15:39:38 +0100

    Chap 5, partie 1 : DropDown + Modifier une catégorie

--- src/Controller/CategorieController.php
+++ src/Controller/CategorieController.php
@@ -2,20 +2,44 @@
 
 namespace App\Controller;
 
+use App\Entity\Categorie;
+use App\Repository\CategorieRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
-use App\Repository\CategorieRepository;
+use App\Form\ModifierCategorieType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 
 final class CategorieController extends AbstractController
 {
     #[Route('/liste-categories', name: 'liste-categories')]
-    public function categorie(CategorieRepository $categorieRepository): Response
+    public function categorie(Request $request,CategorieRepository $categorieRepository,EntityManagerInterface $em): Response
     {
         $categories = $categorieRepository->findAll();
         return $this->render('categorie/liste-categories.html.twig', [
-		'categories' => $categories
-            
+            'categories' => $categories,
+
         ]);
     }
+    #[Route('/modifier-categorie/{id}', name: 'app_modifier_categorie')]
+
+    public function modifierCategorie(Categorie $categorie): Response
+    {
+
+        $form = $this->createForm(ModifierCategorieType::class, $categorie);
+        if($request->isMethod('POST')){
+			$form->handleRequest($request);
+			if ($form->isSubmitted()&&$form->isValid()){
+				$em->persist($categorie);
+				$em->flush();
+				$this->addFlash('notice','Catégorie modifiée');
+				return $this->redirectToRoute('app_liste_categories');
+			}
+		}	
+        return $this->render('categorie/modifier-categorie.html.twig', [
+		'form' => $form->createView()
+        ]);
+    }
+
 }
